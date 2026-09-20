e5ca7aecf2c4e05c45c06bc2614ff26a61460bb0
Silly Cat <silly_cat@debian.lan>
2026-09-20 13:46:09 -0700

    parser

--- main.hs
+++ main.hs
@@ -1,6 +1,6 @@
 data Token
     = Lambda 
-    | Variable Char 
+    | Identifier Char 
     | Seperator
     | OpeningBracket
     | ClosingBracket
@@ -12,10 +12,39 @@ lexch c = case c of
     '.' -> Seperator
     '(' -> OpeningBracket
     ')' -> ClosingBracket
-    _ -> Variable (c)
+    _ -> Identifier (c)
+
+filterWhiteSpace :: String -> String
+filterWhiteSpace text = filter (\x -> not (x == ' ' || x == '\n' || x == '\t')) text
 
 lexer :: String -> [Token]
-lexer code = map lexch code
+lexer code = map lexch (filterWhiteSpace code)
+
+data Function = Function {
+    input :: Char,
+    body :: LambdaTerm
+} deriving (Show)
+
+data LambdaTerm
+    = Variable Char
+    | Abstraction Function
+    | Application Function LambdaTerm
+    | End
+    deriving (Show)
+
+parser :: [Token] -> Int -> LambdaTerm
+parser tokens i = case tokens!!i of
+    Lambda -> Abstraction (Function
+        (case tokens!!(i + 1) of
+            Identifier c -> c
+        ) (parser tokens (i + 3)) )
+    Identifier i -> Variable i
+    Seperator -> error "cant use seperator as expression"
+    OpeningBracket -> parser tokens (i + 1) 
+    ClosingBracket -> End
+
+process :: String -> LambdaTerm
+process code = parser (lexer code) 0
 
 main :: IO ()
 main = putStrLn "Hello World!"
