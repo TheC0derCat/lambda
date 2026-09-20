f6dac8c7301c64798587b4ac1d687c3e8ad0c1f0
Silly Cat <silly_cat@debian.lan>
2026-09-20 11:48:54 -0700

    lexer

--- main.hs
+++ main.hs
@@ -1,1 +1,22 @@
+data Token
+    = Lambda 
+    | Variable Char 
+    | Seperator
+    | OpeningBracket
+    | ClosingBracket
+    deriving (Show)
+
+lexch :: Char -> Token
+lexch c = case c of 
+    'λ' -> Lambda
+    '.' -> Seperator
+    '(' -> OpeningBracket
+    ')' -> ClosingBracket
+    _ -> Variable (c)
+
+lexer :: String -> [Token]
+lexer code = map lexch code
+
+main :: IO ()
 main = putStrLn "Hello World!"
+
